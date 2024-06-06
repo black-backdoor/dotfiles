cf29195740ae84da05dfd6927b9e24d69332d08a
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-06-06 15:36:36 +0200

    Update install.sh
    + log functions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+
+
+# ------- LOG -------
+wait_user () {
+  echo ''
+  printf "[\033[0;33m??\033[0m] $1\n"
+  printf " \033[0;32m>>>\033[0m Press any key to continue"
+  read -n 1 -s -r -p ''
+  echo ''
+}
+
+success () {
+  printf "[\033[00;32mOK\033[0m] $1\n"
+}
+
+fail () {
+  printf "[\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+}
+
+# -------------------
+
+
+
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root."
   exit 1
