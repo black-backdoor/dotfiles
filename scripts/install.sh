3054b33292cc083067133ae34624bfc65a5d8dc3
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-06-06 16:24:05 +0200

    Logic setup symlinks (#1)

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,6 +15,11 @@ success () {
   printf "[\033[00;32mOK\033[0m] $1\n"
 }
 
+warning() {
+  printf "[\033[38;5;208mWARN\033[0m] $1\n"
+  echo ''
+}
+
 fail () {
   printf "[\033[0;31mFAIL\033[0m] $1\n"
   echo ''
@@ -44,3 +49,59 @@ echo " HOME Folder: $HOME"
 
 wait_user "Review the settings above and press any key to continue or Ctrl+C to cancel..."
 
+
+
+echo ''
+echo "---------------- DEPENDENCIES ----------------"
+
+if [ -e $HOME/dotfiles/scripts/dependencies.sh ]; then
+    echo "Found dependencies.sh"
+	chmod +x $HOME/dotfiles/scripts/dependencies.sh
+	
+	echo "installing dependencies"
+	sudo $HOME/dotfiles/scripts/dependencies.sh
+	
+	success "Successfully ran dependencies.sh"
+else
+    warning "dependencies.sh not found. Some features may not work correctly."
+fi
+
+
+
+echo ''
+echo "---------------- SETUP SYMLINKS ----------------"
+#if ! apt install stow -y > /dev/null; then
+#    fail "Error installing 'stow' package"
+#    exit 1
+#fi
+
+
+# ! Remove the existing .dotfiles
+target_dir="$HOME/dotfiles"
+
+
+# List all existing symbolic links pointing to the dotfiles directory
+echo "Existing symbolic links pointing to the dotfiles directory:"
+find "$HOME" -maxdepth 1 -type l -exec sh -c 'readlink -f "$0" | grep -q "^$HOME/dotfiles"' {} \; -print
+
+wait_user "Check the symlinks above and press any key to continue or Ctrl+C to cancel..."
+
+
+# remove all existing symbolic links pointing to the dotfiles directory
+find "$HOME" -maxdepth 1 -type l -exec sh -c 'readlink -f "$0" | grep -q "^$HOME/dotfiles"' {} \; -delete
+
+success "Successfully unlinked symbolic links pointing to $target_dir"
+
+
+
+# ----------------
+
+cd "$HOME/dotfiles" || { fail "Failed to cd $HOME/dotfiles"; exit 1; }
+
+echo "Use 'stow' to create symlinks in the parent directory (user's home directory)"
+if ! stow .; then
+    fail "Error running stow."
+    exit 1
+fi
+
+success "Successfully used 'stow' to create symlinks."
