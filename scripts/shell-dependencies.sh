eb6a7664328faa71ba3fb3793abfdc0ee533aefb
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-06-08 09:30:50 +0200

    Create scripts\shell-dependencies.sh

---
+++ scripts/shell-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# basic-dependencies.sh
+# This script installs the basic dependencies required for additional tools that enhance the user's experience with the bash console.
+
+
+
+# Update the package list
+apt update
+
